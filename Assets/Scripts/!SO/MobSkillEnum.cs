d94fd107e9dae71ddd88ccd770c9e6f39687ff4a
Maksim S <maxsic1985@gmail.com>
2026-08-28 23:09:45 +0300

    add so skills

--- Assets/Scripts/!SO/MobSkillEnum.cs
+++ Assets/Scripts/!SO/MobSkillEnum.cs
@@ -2,7 +2,18 @@
 {
     public enum MobSkillEnum
     {
-        REGEN = 100,
-        POISON = 101
+        GROUP_IQ_KRIT = 100,
+        GROUP_SPEED=101,
+        TARGET_POISON = 102,
+        DEB_ATTACK5 = 103,
+        DAMAGE_REFLECTION=104,
+        ATTACKX2_HALF_HP=105,
+        GROUP_ATTACK=106,
+        STUNN=107,
+        MANA_BURN=108,
+        IQ_X2_HALF_HP=109,
+        DEB_DEFENCE = 110,
+        ATTACKX5_HP_10=111,
+        NO_ESCAPE=112
     }
 }
